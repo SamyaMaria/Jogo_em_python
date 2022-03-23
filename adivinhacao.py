01ae085c59da514c56c1eb03ac1d6a008f702c04
SamyaMaria <saamyamaria@hotmail.com>
2022-03-23 16:49:23 -0300

    .

--- adivinhacao.py
+++ adivinhacao.py
@@ -1,15 +1,44 @@
 import random
 
 def jogar_adivinhacao():
+    mensagem_de_abertura()
+    numero_secreto = random.randrange(1, 101)  # random: para número aleatórios
+    total_de_tentativas = nivel_do_jogo()
+
+    pontos_total = 100
+    for rodada in range(1, total_de_tentativas + 1):
+
+        print("\nTentativa {} de {}" .format(rodada, total_de_tentativas))
+        chute = pede_chute()
+
+        if chute < 1 or chute > 100:
+            print("Você deve digitar um número de 1 a 100 !!!")
+            continue
+
+        if chute == numero_secreto:
+            print("Você acertou!! \nTOTAL DE PONTOS OBTIDOS: {} ".format(pontos_total))
+            break
+        else:
+            if chute >  numero_secreto:
+                print("VOCÊ ERROU ! Seu número foi maior que o número secreto")
+                if rodada == total_de_tentativas:
+                    print("\nVOCÊ PERDEU !! O número secreto era {} e seus pontos obtidos foram: {}". format(numero_secreto, pontos_total))
+            elif chute <  numero_secreto:
+                print("VOCÊ ERROU ! Seu número foi menor que o número secreto")
+                if rodada == total_de_tentativas:
+                    print("\nVOCÊ PERDEU !! O número secreto era {} e seus pontos obtidos foram: {}".format(numero_secreto, pontos_total))
+        pontos = abs(numero_secreto - chute)
+        pontos_total = pontos_total - pontos
+
+    print("\nFIM DO JOGO!!!")
+
+def mensagem_de_abertura():
     print("\n********************************")
     print("Bem vindo ao jogo de adivinhação")
     print("********************************\n")
 
-    numero_secreto = random.randrange(1, 101)  # random: para número aleatórios
+def nivel_do_jogo():
     total_de_tentativas = 0
-    pontos_total = 1000
-    pontos = int
-
     print("ESCOLHA O NÍVEL DO JOGO ")
     print("(1) Fácil   (2) Médio  (3) Difícil")
 
@@ -21,39 +50,12 @@ def jogar_adivinhacao():
         total_de_tentativas = 10
     elif nivel == 3:
         total_de_tentativas = 5
+    return total_de_tentativas
 
-    for rodada in range(1, total_de_tentativas + 1):
+def pede_chute():
+    chute = int(input("Digite um número entre 1 e 100: "))
+    print("Você digitou: ", chute)
+    return chute
 
-        print("\nTentativa {} de {}" .format(rodada, total_de_tentativas))
-
-        chute = int(input("Digite um número entre 1 e 100: "))
-        print("Você digitou ", chute)
-
-        if chute < 1 or chute > 100:
-            print("Você deve digitar um número de 1 a 100 !!!")
-            continue
-
-        acertou = chute == numero_secreto
-        maior   = chute >  numero_secreto
-        menor   = chute <  numero_secreto
-
-        if acertou:
-            print("Você acertou!! \nTOTAL DE PONTOS OBTIDOS: {} ".format(pontos_total))
-            break
-        else:
-            if maior:
-                print("VOCÊ ERROU ! Seu número foi maior que o número secreto")
-                if(rodada == total_de_tentativas):
-                    print("\nVOCÊ PERDEU !! O número secreto era {} e seus pontos obtidos foram: {}". format(numero_secreto, pontos_total))
-            elif menor:
-                print("VOCÊ ERROU ! Seu número foi menor que o número secreto")
-                if rodada == total_de_tentativas:
-                    print("\nVOCÊ PERDEU !! O número secreto era {} e seus pontos obtidos foram: {}".format(numero_secreto, pontos_total))
-            pontos = abs(numero_secreto - chute) #abs == não ficar negativo, constar sempre como inteiro positivo
-            pontos_total = pontos_total - pontos
-
-    print("\nFIM DO JOGO!!!")
-
-
-if __name__ == "__main__": #para executar no promp de comando o arquivo invidualmente sem precisar passar pelo que está integrado.
+if __name__ == "__main__":
     jogar_adivinhacao()
